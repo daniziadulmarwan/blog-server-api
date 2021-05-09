699f80b984440ef1e7ec0198deeb5bf069713b8e
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2021-05-10 02:28:07 +0700

    add

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const AuthController = require("../controllers/AuthController");
-const { validate } = require("../middlewares/Auth");
+const { validate } = require("../middlewares/Validation");
 
 router.post("/register", validate, AuthController.register);
 router.post("/login", AuthController.login);
