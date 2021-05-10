676c8ff6800f5aff9c5667469cddbe90ac6588a3
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2021-05-11 02:41:06 +0700

    add

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -2,8 +2,13 @@ const db = require("../models");
 
 class BlogController {
   index = async (req, res) => {
-    res.send("index blog page");
+    const blogs = await db.blog.findAll();
+    res.status(200).json({
+      message: "success get all blogs",
+      data: blogs,
+    });
   };
+
   create = async (req, res) => {
     const { id, username } = req.app.locals.credential;
     const { title, body } = req.body;
@@ -25,9 +30,63 @@ class BlogController {
       data: blog,
     });
   };
-  show = async (req, res) => {};
-  update = async (req, res) => {};
-  delete = async (req, res) => {};
+
+  show = async (req, res) => {
+    const { id: user_id } = req.app.locals.credential;
+    const { id } = req.params;
+
+    const blog = await db.blog.findOne({
+      where: { id, user_id },
+    });
+    if (!blog) {
+      return res.status(401).json({
+        message: "blog not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "success get blog",
+      data: blog,
+    });
+  };
+
+  update = async (req, res) => {
+    const { id: user_id } = req.app.locals.credential;
+    const { id } = req.params;
+    const { title, body } = req.body;
+
+    const blog = await db.blog.update(
+      {
+        title,
+        body,
+      },
+      {
+        where: { id, user_id },
+      }
+    );
+    if (!blog) {
+      return res.status(401).json({
+        message: "update blog failed",
+      });
+    }
+
+    return res.status(200).json({
+      message: "success update blog",
+    });
+  };
+
+  delete = async (req, res) => {
+    const { id: user_id } = req.app.locals.credential;
+    const { id } = req.params;
+
+    await db.blog.destroy({
+      where: { id, user_id },
+    });
+
+    return res.status(200).json({
+      message: "success delete blog",
+    });
+  };
 }
 
 module.exports = new BlogController();
