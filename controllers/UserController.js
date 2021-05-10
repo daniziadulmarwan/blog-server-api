5863770719585df4fa73408ca2125840178e113c
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2021-05-10 13:57:17 +0700

    add

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -12,9 +12,59 @@ class UserController {
     });
   };
 
-  show(req, res) {}
-  update(req, res) {}
-  delete(req, res) {}
+  show = async (req, res) => {
+    const { id } = req.params;
+
+    const user = await db.user.findOne({
+      where: { id: id },
+    });
+    if (!user) {
+      return res.status(422).json({
+        message: "user not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "success get user",
+      data: user,
+    });
+  };
+  update = async (req, res) => {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    const user = await db.user.update(
+      {
+        username,
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    if (!user) {
+      return res.status(422).json({
+        message: "user not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "success update user",
+      data: user,
+    });
+  };
+  delete = async (req, res) => {
+    const { id } = req.params;
+
+    await db.user.destroy({
+      where: { id: id },
+    });
+
+    return res.status(200).json({
+      message: "success delete data",
+    });
+  };
 }
 
 module.exports = new UserController();
